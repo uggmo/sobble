5225521f9f8b8e7797deaab53dfeaf13883160b5
uggmo <48570478+uggmo@users.noreply.github.com>
2019-11-19 19:30:34 +0000

    Update SobbleBot.py

--- SobbleBot.py
+++ SobbleBot.py
@@ -2,11 +2,9 @@ import discord
 import asyncio
 import random
 import time
-import dbl
 import logging
 from discord import Client
 from discord.ext import commands
-
 bot = commands.Bot(command_prefix='sb')
 bot.remove_command('help')
 @bot.event
@@ -128,30 +126,5 @@ async def unmute(ctx, member : discord.Member, * , reason=None):
  await member.unmute(reason=reason)
  await ctx.send("unzip! That user's been unmuted")
 
-#dbl
-class DiscordBotsOrgAPI(commands.Cog):
-    """Handles interactions with the top.gg API"""
 
-    def __init__(self, bot):
-        self.bot = bot
-        self.token = 'dbl_token' # set this to your DBL token
-        self.dblpy = dbl.Client(self.bot, self.token)
-        self.updating = self.bot.loop.create_task(self.update_stats())
-
-    async def update_stats(self):
-        """This function runs every 30 minutes to automatically update your server count"""
-        while not self.bot.is_closed():
-            logger.info('Attempting to post server count')
-            try:
-                await self.dblpy.post_guild_count()
-                logger.info('Posted server count ({})'.format(self.dblpy.guild_count()))
-            except Exception as e:
-                logger.exception('Failed to post server count\n{}: {}'.format(type(e).__name__, e))
-            await asyncio.sleep(1800)
-
-def setup(bot):
-    global logger
-    logger = logging.getLogger('bot')
-    bot.add_cog(DiscordBotsOrgAPI(bot))
 bot.run('NjQ1MDA5Njc4MjI0NDU3NzQw.XdRAoQ.m2WxInCAssliDtGXt9akAp8766A')
-
